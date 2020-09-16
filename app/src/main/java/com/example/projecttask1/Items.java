8c6458a811768338b431897ee91a7970b92c3fbf
drishtyjha1 <45451398+drishtyjha1@users.noreply.github.com>
2020-09-17 02:06:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/projecttask1/Items.java
@@ -0,0 +1,10 @@
+package com.example.projecttask1;
+
+public class Items {
+
+    public int itemID;
+    public String itemName;
+    public boolean isChecked;
+
+}
+
---
+++ app/src/main/java/com/example/projecttask1/ReminderItem.java
@@ -0,0 +1,10 @@
+package com.example.projecttask1;
+
+import java.util.ArrayList;
+
+public class ReminderItem {
+
+    public int id;
+    public String title;
+    public String items;
+}
